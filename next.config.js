95020fa1fc52a09c69d5efd70d4cc91ce565a2ae
Muzammil Rafiq <60405801+muzammil17sp@users.noreply.github.com>
2021-11-04 15:47:54 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,5 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "a0.muscache.com"],
   },
-  env: {
-    mapbox_key:
-      "pk.eyJ1IjoibXV6YW1taWwxNDQiLCJhIjoiY2tzMmMyamttMWt6NzJwbzlmMWc4ZWU4eiJ9.fE1faL8Jrv3EybndaO30LA",
-  },
+  
 };
